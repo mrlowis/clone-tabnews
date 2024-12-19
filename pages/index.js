df008ee7f551bada5bc49df9989ab37097427837
mrlowis <luizfernandogomesvale@gmail.com>
2024-12-19 01:27:33 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home () {
-    return <h1>Vai tomar no cu Portal RPG! Vem pro UDM, porra!</h1>
+function Home() {
+    return (
+        <div>
+            <h1>Vai tomar no cu Portal RPG!</h1>
+            <p>
+                Vem pro <a href="https://universodomedo.com">UDM</a>, porra!
+            </p>
+        </div>
+    );
 }
 
 export default Home;
